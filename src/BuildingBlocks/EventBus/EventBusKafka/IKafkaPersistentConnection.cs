ba276c2f42e13b7a346a8f1e2bf9a02f488e17e0
skidrow406 <skidrow406@gmail.com>
2021-08-17 16:43:56 +0700

    commit

---
+++ src/BuildingBlocks/EventBus/EventBusKafka/IKafkaPersistentConnection.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+using System;
+using Confluent.Kafka;
+
+namespace Microsoft.eShopOnContainers.BuildingBlocks.EventBusKafka
+{
+    public interface IKafkaPersistentConnection
+        : IDisposable
+    {
+        bool IsConnected { get; }
+
+        bool TryConnect();
+
+        IModel CreateModel();
+    }
+}
---
+++ src/Services/BuyOrder/BuyOrder.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuyOrder.FunctionalTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
